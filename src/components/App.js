8b1c2631e5e23dbdc6d9d28b6fe64c2a13d0f4d6
sanjay <sanjaysighania7@gmail.com>
2021-08-17 13:05:08 +0530

    class done

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,17 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+		this.state={
+			phidden:true,
+		}
 	};
 
     render() {
     	return(
     		<div id="main">
-				{ /* Do not remove this main div!! */ }
+				<button id="click" onClick={()=>this.setState({phidden:false})}>show para</button>
+
+				{this.state.phidden ? null : <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
     		</div>
     	);
     }
